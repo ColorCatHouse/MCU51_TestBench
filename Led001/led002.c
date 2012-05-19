92977e9b9a82e9d62204b2044a1028eaf675dbd0
April <starkieblue@gmail.com>
2012-05-19 17:49:29 +0800

    blinking leds

--- Led001/led002.c
+++ Led001/led002.c
@@ -7,8 +7,14 @@ unsigned char led(unsigned char a)
 
 void main(void)
 {
+	unsigned int	t;
+
 	for(;;)
 	{
 		P0 = led(0x43);
+		for (t=0; t<10000; t++);
+		
+		P0 = led(0x00);
+		for (t=0; t<10000; t++);
 	}
 }
