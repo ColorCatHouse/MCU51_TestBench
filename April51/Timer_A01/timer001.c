9c7e6b82aab8189d0353fe6d29880580ad864988
April <starkieblue@gmail.com>
2012-06-18 00:07:16 +0800

    Dad show me the usage of timer

---
+++ April51/Timer_A01/timer001.c
@@ -0,0 +1,29 @@
+#include <STC89.H>
+
+unsigned char pattern;
+
+void main(void)
+{
+	TH0=0;
+	TL0=0;
+
+	TMOD=T0_M1;
+	
+	ET0=1;
+	EA=1;
+
+	TR0=1;
+
+	for (;;)
+	{
+		P0=~pattern;
+	}
+}
+
+void isrTimer(void) interrupt 1
+{
+	pattern++;
+
+	TH0=0;
+	TL0=0;
+}
